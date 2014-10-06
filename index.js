c9819a9623d421bd561661ae643e7ef5b1fd2098
Kevin Martensson <kevinmartensson@gmail.com>
2014-10-06 21:38:43 +0200

    Use xdg-trashdir
    Fixes #1.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var each = require('each-async');
 var fs = require('fs');
 var path = require('path');
 var rm = require('rimraf');
-var home = require('xdg-basedir').data;
+var trashdir = require('xdg-trashdir');
 
 /**
  * Empty trash on Linux
@@ -20,46 +20,53 @@ module.exports = function (cb) {
 		cb(new Error('Only Linux systems are supported'));
 	}
 
-	var paths = [
-		path.join(home, 'Trash', 'files'),
-		path.join(home, 'Trash', 'info')
-	];
-
-	each(paths, function (p, i, next) {
-		fs.readdir(p, function (err, files) {
-			if (err) {
-				cb(err);
-				return;
-			}
-
-			files = files.map(function (file) {
-				return path.join(p, file);
-			});
-
-			each(files, function (file, i, n) {
-				rm(file, function (err) {
-					if (err) {
-						n(err);
-						return;
-					}
-
-					n();
-				});
-			}, function (err) {
-				if (err) {
-					next(err);
-					return;
-				}
-
-				next();
-			});
-		});
-	}, function (err) {
+	trashdir(function (err, dir) {
 		if (err) {
 			cb(err);
 			return;
 		}
 
-		cb();
+		var paths = [
+			path.join(dir, 'files'),
+			path.join(dir, 'info')
+		];
+
+		each(paths, function (p, i, next) {
+			fs.readdir(p, function (err, files) {
+				if (err) {
+					cb(err);
+					return;
+				}
+
+				files = files.map(function (file) {
+					return path.join(p, file);
+				});
+
+				each(files, function (file, i, n) {
+					rm(file, function (err) {
+						if (err) {
+							n(err);
+							return;
+						}
+
+						n();
+					});
+				}, function (err) {
+					if (err) {
+						next(err);
+						return;
+					}
+
+					next();
+				});
+			});
+		}, function (err) {
+			if (err) {
+				cb(err);
+				return;
+			}
+
+			cb();
+		});
 	});
 };
